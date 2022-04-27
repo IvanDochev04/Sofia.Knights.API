935c6051e5c36e83ca53cbca10ca23bb24134e6a
Ivan Dochev <ivan.dochev@accedia.com>
2022-04-27 11:47:24 +0300

    Add users

---
+++ SofiaKnights-API/Accounts/Mappings/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using SofiaKnights_API.Accounts.Entities.DTOs;
+using SofiaKnights_API.Accounts.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SofiaKnights_API.Accounts.Mappings
+{
+    public class MappingProfile : Profile
+
+    {
+        public MappingProfile()
+        {
+            CreateMap<UserForRegistrationDto, User>()
+    .ForMember(u => u.UserName, opt => opt.MapFrom(x => x.Email));
+        }
+    }
+}
